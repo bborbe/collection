bcbeda0e4f6157550b39a3b43a454a1450db85dc
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-01-04 10:02:30 +0100

    add Equal and ContainsAll

---
+++ contains-all_test.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2024 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package collection_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/bborbe/collection"
+)
+
+var _ = DescribeTable("ContainsAll",
+	func(a []string, b []string, expectedResult bool) {
+		Expect(collection.ContainsAll(a, b)).To(Equal(expectedResult))
+	},
+	Entry("empty", []string{}, []string{}, true),
+	Entry("multiple", []string{"a", "b", "c"}, []string{"a", "b", "c"}, true),
+	Entry("same length", []string{"a"}, []string{"b"}, false),
+	Entry("different length", []string{"a"}, []string{"b", "c"}, false),
+	Entry("different length", []string{"a"}, []string{"a", "a"}, true),
+	Entry("different length", []string{"a", "a"}, []string{"a"}, true),
+)
